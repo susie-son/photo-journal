492e6edd8edf7da27758f7730c31e525b5f8f9ea
Susie Son <susieson99@gmail.com>
2020-12-25 16:34:34 -0500

    add camera permission

--- app/src/main/java/com/susieson/food/common/Constants.kt
+++ app/src/main/java/com/susieson/food/common/Constants.kt
@@ -1,3 +1,4 @@
 package com.susieson.food.common
 
 const val RC_SIGN_IN = 1
+const val RC_CAMERA = 2
--- app/src/main/java/com/susieson/food/ui/main/picture/PictureFragment.kt
+++ app/src/main/java/com/susieson/food/ui/main/picture/PictureFragment.kt
@@ -1,15 +1,21 @@
 package com.susieson.food.ui.main.picture
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.susieson.food.R
+import com.susieson.food.common.RC_CAMERA
 import com.susieson.food.databinding.FragmentPictureBinding
+import pub.devrel.easypermissions.EasyPermissions
+import timber.log.Timber
 
-class PictureFragment : Fragment() {
+class PictureFragment : Fragment(), EasyPermissions.PermissionCallbacks,
+    EasyPermissions.RationaleCallbacks {
     private lateinit var binding: FragmentPictureBinding
 
     override fun onCreateView(
@@ -17,6 +23,56 @@ class PictureFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_picture, container, false)
+        showCamera()
         return binding.root
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        when (requestCode) {
+            RC_CAMERA -> {
+                showCamera()
+            }
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        when (requestCode) {
+            RC_CAMERA -> {
+                if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+                    Timber.d("camera permission permanently denied")
+                }
+            }
+        }
+        findNavController().navigateUp()
+    }
+
+    override fun onRationaleAccepted(requestCode: Int) {
+
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
+        findNavController().navigateUp()
+    }
+
+    private fun showCamera() {
+        if (EasyPermissions.hasPermissions(requireContext(), Manifest.permission.CAMERA)) {
+            Timber.d("camera permission granted")
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                getString(R.string.camera_permission_rationale),
+                RC_CAMERA,
+                Manifest.permission.CAMERA
+            )
+        }
     }
 }
